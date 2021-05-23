8b444a93411f6e0cf163591bd8f6b446443d9e24
Polina Ponomareva <polina.ponomareva@skoltech.ru>
2021-05-23 13:28:13 +0300

    evaluation of the configuration with cube orientation

--- enjoy_ur_grasping.py
+++ enjoy_ur_grasping.py
@@ -3,6 +3,7 @@ import imageio
 import numpy as np
 from stable_baselines import PPO2
 from stable_baselines.common.vec_env import DummyVecEnv
+import time
 
 env = gym.make('ur_env:ur-v0')
 env_vec = DummyVecEnv([lambda: env])
@@ -33,9 +34,12 @@ def evaluate_performance():
         caught.append(curCaught)
         lifted.append(curLifted)
         moved.append(curMoved)
-    print(f'caught: {caught}')
-    print(f'lifted: {lifted}')
-    print(f'moved: {moved}')
+    caught = np.asarray(caught)
+    lifted = np.asarray(lifted)
+    moved = np.asarray(moved)
+    print(f'caught cubes with orientation: {len(caught[caught == True])} out of {numIterations} -- {len(caught[caught == True]) / numIterations} %')
+    print(f'lifted cubes with orientation: {len(lifted[lifted == True])} out of {numIterations} -- {len(lifted[lifted == True]) / numIterations} %')
+    print(f'moved cubes with orientation: {len(moved[moved == True])} out of {numIterations} -- {len(moved[moved == True]) / numIterations} %')
 
 
 # obs = env.reset()
@@ -48,7 +52,10 @@ def evaluate_performance():
 #     print(f'info: {info}')
 #     env.render(mode='human')
 
-
+startTime = time.time()
 evaluate_performance()
+endTime = time.time()
+elapsedTime = endTime - startTime
+print(f'Elapsed time: {elapsedTime / 60.} minutes')
 
 
